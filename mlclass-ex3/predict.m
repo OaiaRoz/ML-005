13166cf9cc63dae3c00bd79bb2dc220aa5419067
Dan Fundatureanu <fdan_grigore@yahoo.com>
2014-04-24 16:59:35 -0400

    update finished for EX3

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -23,8 +23,15 @@ p = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
+Z2 = X*Theta1';
+A2 = sigmoid(Z2);
 
+A2 = [ones(m, 1) A2];
+Z3 = A2*Theta2';
+A3 = sigmoid(Z3);
 
+[b, ip] = max(A3');
+p = ip';
 % =========================================================================
 
 
