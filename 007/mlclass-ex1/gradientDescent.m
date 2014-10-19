d5cc002a1470fd443fc99467f3c84993a213b81a
Dan Fundatureanu <dan.fundatureanu@gmail.com>
2014-10-19 15:16:13 -0400

    partial solved

--- 007/mlclass-ex1/gradientDescent.m
+++ 007/mlclass-ex1/gradientDescent.m
@@ -16,13 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+	tmp_1 = theta(1) - alpha*(1/m)*sum( (X*theta .- y) .* X(:,1) );
+	tmp_2 = theta(2) - alpha*(1/m)*sum( (X*theta .- y) .* X(:,2) );
+	
+	theta = [tmp_1; tmp_2];
     % ============================================================
 
     % Save the cost J in every iteration    
