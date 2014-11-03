07d096f826492c06ef3e5376e195c02ebbb7eaa1
Dan Fundatureanu <fdan_grigore@yahoo.com>
2014-11-02 21:25:29 -0500

    3 and 4 done!

--- 007/mlclass-ex3/predictOneVsAll.m
+++ 007/mlclass-ex3/predictOneVsAll.m
@@ -29,13 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
-
+[b, ip] = max( (sigmoid(X*all_theta'))' );
+p = ip';
 % =========================================================================
 
 
